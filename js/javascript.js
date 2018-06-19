0abfe866b4673dd2187af41a5cbe123d6c4ab986
Wessel <38309266+Wesselvdp@users.noreply.github.com>
2018-06-19 14:21:48 +0200

    Add files via upload
    first commit

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	var elem1 = document.getElementById('laptop-preview').style;
+	var elem2 = document.getElementById('btnSignup').style;
+	elem1.transition = "background-position-y 0.9s ease 0s, opacity 0.9s ease 0s";
+	elem1.opacity = "1";
+	elem1.backgroundPositionY = "21.6px";
+	
+	elem2.transition = "margin-left 0.6s ease-out 0s";
+	elem2.marginLeft = "inherit";
+})
+	
